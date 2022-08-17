dee9a74cc6631cbd7948aa711c3602fce3235c7a
Usuario <Usuario@CPD02-M13>
2022-08-17 15:50:14 -0300

    Adicionar arquivos de projeto.

---
+++ AppLogin/AppLogin/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using AppLogin.Model;
+using AppLogin.View;
+
+namespace AppLogin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Login());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
